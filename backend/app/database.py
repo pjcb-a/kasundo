d8019a153835206ecead9660c353cb87aed7a448
pjcb-a <johnpauljacob15@gmail.com>
2026-06-06 15:40:58 +0800

    feat: added security and service setup

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,15 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-import os 
-from dotenv import load_dotenv
 
-DATABASE_URL = (
-        os.getenv("DATABASE_URL")
-)
+from app.config import settings
+
+DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(
     DATABASE_URL,
-    echo=True
+    echo=False
 )
 
 SessionLocal = sessionmaker(
